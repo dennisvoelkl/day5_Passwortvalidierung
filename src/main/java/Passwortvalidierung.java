63d3a6801e9d3e6732edd3b89ee30d5198aad273
voelkldennis <voelkldennis@googlemail.com>
2022-09-30 10:08:02 +0200

    Add Test Class

--- src/main/java/Passwortvalidierung.java
+++ src/main/java/Passwortvalidierung.java
@@ -1,5 +1,22 @@
+/*
+Aufgabe: Passwortvalidierung
+Schreibe eine Passwortvalidierung.
+
+1. Lege dir ein neues Java Github Projekt an (OK)
+2. Überlege dir, wie die Methoden aussehen müssen.
+3. Schreibe jeweils erst einen Test und dann die Funktionalität
+4. Überprüfe erst die Länge des Passworts und dann, ob Zahlen enthalten sind
+ */
 public class Passwortvalidierung {
     public static void main(String[] args) {
-        
+
+
     }
+
+    //Methode für Passworteingabe
+    //  inputPassword()
+    //
+
+
 }
+
